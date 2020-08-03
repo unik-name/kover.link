4c92b30b8d19ea6841d6fb3c40f1824d1cbbe46c
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-08-03 15:29:07 +0200

    chore: header and footer links

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -116,7 +116,7 @@ const Header: FC = () => {
           >
             <Li>
               <ALink
-                href="//github.com/thedevs-network/kutt"
+                href="//github.com/spacelephant/kover.link"
                 target="_blank"
                 rel="noopener noreferrer"
                 title="GitHub"
